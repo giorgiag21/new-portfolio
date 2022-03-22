f65c58433576420f31ff66fff59ee75c9a19d29e
giorgiag21 <giorgia.galbiati90@gmail.com>
2022-03-22 16:12:35 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-const arrow = document.getElementsByClassName("arrow");
-const viewWorkButton = document.getElementsByClassName("button");
+// const toggleButton = document.getElementsByClassName("toggle-button")[0];
+// const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+// const arrow = document.getElementsByClassName("arrow");
+// const viewWorkButton = document.getElementsByClassName("button");
 
-toggleButton.addEventListener("click", function () {
-  navbarLinks.classList.toggle("active");
-});
+// toggleButton.addEventListener("click", function () {
+//   navbarLinks.classList.toggle("active");
+// });
+
+const navSlide = () => {
+  const toggleButton = document.querySelector(".toggle-button");
+  const navbarLinks = document.querySelector(".navbar-links");
+
+  toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  });
+};
+
+navSlide();
