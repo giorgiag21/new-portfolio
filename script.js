20185ac667a7ea03715850e9bec41cd6c2a86fe9
giorgiag21 <giorgia.galbiati90@gmail.com>
2022-03-25 15:42:35 +0100

    Added contact form and styled footer

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
-// const toggleButton = document.getElementsByClassName("toggle-button")[0];
-// const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-// const arrow = document.getElementsByClassName("arrow");
-// const viewWorkButton = document.getElementsByClassName("button");
-
-// toggleButton.addEventListener("click", function () {
-//   navbarLinks.classList.toggle("active");
-// });
-
-const navSlide = () => {
+// NAVBAR
+function navSlide() {
   const toggleButton = document.querySelector(".toggle-button");
   const navbarLinks = document.querySelector(".navbar-links");
 
   toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active");
   });
-};
-
+}
 navSlide();
+
+// // CONTACT FORM
+// // Get data
+// const nameInput = document.querySelector("#name");
+// const email = document.querySelector("#email");
+// const message = document.querySelector("#message");
+// const success = document.querySelector("#success");
+// const errorNodes = document.querySelectorAll(".error");
+
+// // Validate data
+// function validateForm() {
+//   clearMessages();
+//   let errorFlag = false;
+
+//   if (nameInput.value.length < 1) {
+//     errorNodes[0].innerText = "Name cannot be blank";
+//     nameInput.classList.add("error-border");
+//     errorFlag = true;
+//   }
+
+//   if (!emailIsValid(email.value)) {
+//     errorNodes[1].innerText = "Invalid email address";
+//     email.classList.add("error-border");
+//     errorFlag = true;
+//   }
+
+//   if (message.value.length < 1) {
+//     errorNodes[2].innerText = "Please enter a message";
+//     message.classList.add("error-border");
+//     errorFlag = true;
+//   }
+
+//   if (!errorFlag) {
+//     success.innerText = "Success!";
+//   }
+// }
+
+// // Clear error / success message
+// function clearMessages() {
+//   for (let i = 0; i < errorNodes.lenght; i++) {
+//     errorNodes[i].innerText = "";
+//   }
+//   success.innerText = "";
+//   nameInput.classList.remove("error-border");
+//   email.classList.remove("error-border");
+//   message.classList.remove("error-border");
+// }
+
+// // Check if email is valid
+// function emailIsValid(email) {
+//   let pattern = /\S+@\S+\.\S+/;
+//   return pattern.test(email);
+// }
