7a251db506d082a6615d55fee01fa259c4a733e8
giorgiag21 <giorgia.galbiati90@gmail.com>
2022-03-28 17:42:42 +0200

    Implemented responsiveness

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 // NAVBAR
-function navSlide() {
-  const toggleButton = document.querySelector(".toggle-button");
-  const navbarLinks = document.querySelector(".navbar-links");
+const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar-links");
+let menuOpen = false;
 
-  toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
-  });
-}
-navSlide();
+toggleButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  navbarLinks.classList.toggle("active");
+  if (!menuOpen) {
+    toggleButton.classList.add("open");
+    menuOpen = true;
+  } else {
+    toggleButton.classList.remove("open");
+    menuOpen = false;
+  }
+});
 
+const navbar = document.querySelector(".navbar");
+let offsetY = 100;
+
+window.addEventListener("scroll", () => {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset > offsetY) {
+    navbar.classList.remove("active");
+    navbarLinks.classList.remove("active");
+  } else {
+    navbar.classList.add("active");
+  }
+  offsetY = window.pageYOffset > 100 ? window.pageYOffset : 100;
+});
