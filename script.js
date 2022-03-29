72374f8bca90b55a3d3e90e791cf15dc4fdb6205
giorgiag21 <giorgia.galbiati90@gmail.com>
2022-03-29 19:18:57 +0200

    Animated footer icons

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ const navbar = document.querySelector(".navbar");
 let offsetY = 100;
 
 window.addEventListener("scroll", () => {
-  console.log(window.pageYOffset);
   if (window.pageYOffset > offsetY) {
     navbar.classList.remove("active");
     navbarLinks.classList.remove("active");
@@ -30,3 +29,15 @@ window.addEventListener("scroll", () => {
   }
   offsetY = window.pageYOffset > 100 ? window.pageYOffset : 100;
 });
+
+const contactIcon = document.querySelectorAll(".contact-icon");
+console.log(contactIcon);
+
+for (let i = 0; i < contactIcon.length; i++) {
+  contactIcon[i].addEventListener("mouseover", () => {
+    contactIcon[i].classList.add("fa-bounce");
+  });
+  contactIcon[i].addEventListener("mouseleave", () => {
+    contactIcon[i].classList.remove("fa-bounce");
+  });
+}
